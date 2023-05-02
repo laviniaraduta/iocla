a0a3ae41b0c6bf5f2feb3289c0c2fe95201055a1
laviniaraduta <lavinia.maria.raduta07@gmail.com>
2023-05-02 12:19:05 +0300

    laborator 07

--- laborator/content/structuri-vectori-siruri/0-fibonacci/fibo-sum.asm
+++ laborator/content/structuri-vectori-siruri/0-fibonacci/fibo-sum.asm
@@ -14,8 +14,23 @@ main:
     ; TODO: calculate the sum of first N fibonacci numbers
     ;       (f(0) = 0, f(1) = 1)
     xor eax, eax     ;store the sum in eax
+    xor ebx, ebx       ;store the first number in ebx
+    xor edx, edx       ;store the second number in edx
+    mov edx, 1
 
     ; use loop instruction
+    xor ecx, ecx
+    mov ecx, dword[N]
+    sub ecx, 2
+    add eax, 1
+
+fib:
+    mov edi, ebx
+    add edi, edx ; in edi we have the new number
+    mov ebx, edx
+    mov edx, edi
+    add eax, edi
+    loop fib
 
     push eax
     push dword [N]
@@ -26,3 +41,7 @@ main:
     xor eax, eax
     leave
     ret
+
+
+
+
